5f33f22e1ff095eddcf525839ec59a622c0abbab
hamany14 <109864236+hamany14@users.noreply.github.com>
2022-11-02 19:32:14 +0300

    done

--- script.js
+++ script.js
@@ -8,20 +8,20 @@ let random_grade_div = document.getElementById("random-grade");
 // لا تقم بتغيير او تخريب الكود الموجود فوق هذا الكومنت
 
 // طبّق الخطوة 4 في أسفل هذا الكومنت
-
+grades.pop()
 // طبّق الخطوة 5 في أسفل هذا الكومنت
-
+grades.push(99)
 // طبّق الخطوة 6 أسفل  هذا الكومنت
-
-
+for (let grade = 0; grade < grades.length; grades++) {
+  text += grades[grade] ;
+} 
 
 function search() {
-  
   // طبّق الخطوة 7و8 أسفل هذا الكومنت
-  
-  
+  let randomnumber = Math.floor(Math.random()*grades.length);
   // طبّق الخطوة 9 أسفل هذا الكومنت
-
+ random_grade_div.innerHTML = `<div>${grades[randomnumber]}</div>`;
+ random_grade_div.style.backgroundColor = "white"
 }
 
 
